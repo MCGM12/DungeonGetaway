a4c96cccc3c5677c7de261a86d9161fc85eb8270
Andrew1542 <andrewariasayala@gmail.com>
2021-04-16 16:50:57 -0700

    Update Prefabs.meta, PlayerTest.unity, and 5 more files...

--- Assets/Scripts/Player Movement/Controller.cs
+++ Assets/Scripts/Player Movement/Controller.cs
@@ -6,21 +6,25 @@ public class Controller : MonoBehaviour
 {
     public float moveSpeed = 6f;
 
-    Rigidbody2D rb;
-   //public Camera viewCamera;
+    Vector2 movement;
+
+    private Rigidbody2D rb;
+  
     Vector3 velocity;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-      //  viewCamera = Camera.main;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * moveSpeed;
+
+     
     }
 
     private void FixedUpdate()
