7516b9bb56c3e1d7c6c5b5f9c44243df760954f4
Andrew1542 <andrewariasayala@gmail.com>
2021-04-19 13:11:19 -0700

    Update FireBall.png, FireBall.png.meta, and 13 more files...

---
+++ Assets/Scripts/Player Movement/FireBall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    public float speed;
+    public int damage;
+
+    void Start()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+        Debug.Log("Get Sniped ");
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
