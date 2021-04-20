fa58a417729b9aecd7e25be8f6f6c9c2192a69c8
MCGM12 <matocraft506@gmail.com>
2021-04-20 15:33:16 -0700

    Update Controller.cs and UI.meta

--- Assets/Scripts/Player Movement/Controller.cs
+++ Assets/Scripts/Player Movement/Controller.cs
@@ -5,41 +5,24 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     public float moveSpeed = 6f;
-
     Vector2 movement;
-
-<<<<<<< HEAD
-    Vector2 movement;
-
     private Rigidbody2D rb;
-<<<<<<< HEAD
    //public Camera viewCamera;
-=======
-  
->>>>>>> main
-=======
-    private Rigidbody2D rb;
-  
->>>>>>> main
     Vector3 velocity;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
+
         velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * moveSpeed;
 
-=======
         velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * moveSpeed;
 
->>>>>>> main
      
     }
 
