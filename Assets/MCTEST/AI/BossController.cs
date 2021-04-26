b2987b357093ae5127386041e3b00a2d2a2cbd37
Matthew Cook <matocraft506@gmail.com>
2021-04-26 13:34:10 -0700

    Update BossController.cs, BossController.cs.meta, and UI.meta

---
+++ Assets/MCTEST/AI/BossController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BossController : MonoBehaviour
+{
+    [SerializeField] private UnityEvent myTestTrigger;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            myTestTrigger.Invoke();
+            Debug.Log("Invoking Test Event!");
+        }
+    }
+}
