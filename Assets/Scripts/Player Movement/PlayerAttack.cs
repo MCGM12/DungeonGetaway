95a388b7eacd31fcb39f7a05a336c92f210ee053
Andrew1542 <andrewariasayala@gmail.com>
2021-04-27 18:47:28 -0700

    Update PlayerTest.unity, PlayerAttack.cs, and 2 more files...

---
+++ Assets/Scripts/Player Movement/PlayerAttack.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public Transform AttackPos;
+    public LayerMask whatIsEnemies;
+    public float attackRange;
+
+    void Update()
+    {/*
+        if (timeBtwAttack <= 0)
+        {
+            if (Input.GetKey(KeyCode.Mouse1))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(AttackPos.position, attackRange,whatIsEnemies );
+
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<EnemyHealth>()
+                }
+            }
+            timeBtwAttack = startTimeBtwAttack;
+        }
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+        
+        
+    }
+        */
+    }
+
+
+
+}
