aa2ca28cff1a387a1e22e0685eb968186b35be93
Matthew Cook <matocraft506@gmail.com>
2021-05-03 13:30:57 -0700

    Update PlayerTest.unity and DashMovement.cs

--- Assets/Scripts/Player Movement/DashMovement.cs
+++ Assets/Scripts/Player Movement/DashMovement.cs
@@ -6,9 +6,13 @@ public class DashMovement : MonoBehaviour
 {
     public Rigidbody2D Rgb2d;
 
+    private Vector2 mouseSpot;
+
     public int dashSpeed = 200;
 
     bool dash = false;
+
+    bool mousePos = true;
 
   public  int dashCooldown = 80;
 
@@ -20,6 +24,7 @@ public class DashMovement : MonoBehaviour
         if (dashCooldown == 0)
         {
             dash = true;
+            mousePos = true;
         }
         else
         {
@@ -34,10 +39,18 @@ public class DashMovement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.LeftShift) && dash == true)
         {
+            if(mousePos)
+            {
+                mouseSpot = Input.mousePosition;
+                mousePos = false;
+            }
+            
             Rgb2d.velocity = Vector2.zero;
             Vector2 mouseDirection = (Input.mousePosition - new Vector3(Screen.width / 2, Screen.height / 2)).normalized;
 
             Rgb2d.AddForce(mouseDirection * dashSpeed * Time.deltaTime*2);
+
+            //Rgb2d.MovePosition(Vector2.Lerp(mouseDirection, mouseSpot, dashSpeed * Time.deltaTime));
             dash = false;
             dashCooldown = 80;
         }
