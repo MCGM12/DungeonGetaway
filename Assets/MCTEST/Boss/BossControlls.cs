68d030b41cf1f27fd6b15a40436e2da1484680a7
Matthew Cook <matocraft506@gmail.com>
2021-05-18 11:22:57 -0700

    Update BossControlls.cs

--- Assets/MCTEST/Boss/BossControlls.cs
+++ Assets/MCTEST/Boss/BossControlls.cs
@@ -6,25 +6,26 @@ using UnityEngine;
 
 public class BossControlls : StateMachineBehaviour
 {
-    
+    public float speed = 5f;
+    public Transform player;
 
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        player = GameObject.Find("Player").transform;
 
     }
 
-    //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        Vector2 direction = player.position - animator.transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        rotation.x = player.rotation.x;
+        rotation.y = player.rotation.y;
+        animator.transform.rotation = Quaternion.Slerp(animator.transform.rotation, rotation, speed * Time.deltaTime);
     }
 
-    //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-
-    }
+  
 
 
     
