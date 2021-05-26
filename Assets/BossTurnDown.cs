1e47532f9bd5a38da22b20562fc6f54fb24cf99d
MCGM12 <matocraft506@gmail.com>
2021-05-26 13:31:28 -0700

    Update BossTurnDown.cs and BossTurnDown.cs.meta

---
+++ Assets/BossTurnDown.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Pathfinding;
+
+public class BossTurnDown : StateMachineBehaviour
+{
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.GetComponent<AIPath>().slowWhenNotFacingTarget = true;
+        animator.GetComponent<AIPath>().rotationSpeed = 180;
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    
+}
